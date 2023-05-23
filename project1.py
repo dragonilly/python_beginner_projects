c9ecbee24b5edc154df590a6b7214bc0a8f584e7
dragonilly <134251470+dragonilly@users.noreply.github.com>
2023-05-23 11:54:59 +0545

    Create project1.py
    madlibs project

---
+++ project1.py
@@ -0,0 +1,16 @@
+# lets create madlibs
+
+
+verb=input("enter verb  in ing ")
+verb2=input("enter verb")
+name=input("enter name of mountain ")
+name1=input("enter name of sampoo ")
+food=input("enter food name")
+
+
+
+madlibs=f"Hello I am a boy who likes magic and I love to {verb} from mountain name {name} after {verb}.\
+i bath using {name1} shampoo and go with my friend in mountain to {verb2} and finally come\
+home for {food} only not my home I go to the home of the stranger "
+       
+print(madlibs)
